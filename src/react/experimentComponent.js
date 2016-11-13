3bcab1514699743b7e727a5a9c20841e2779acd8
Adam Faryna <adamfaryna@gmail.com>
2016-11-13 18:13:29 +0000

    Added react component.

--- src/react/experimentComponent.js
+++ src/react/experimentComponent.js
@@ -1,3 +1,4 @@
+/** @jsx React.DOM */
 app.react.ExperimentComponent = (function () {
 	var startTime;
 
@@ -19,7 +20,34 @@ app.react.ExperimentComponent = (function () {
 			startTime = Date.now();
 
 			var elements = this.props.data.map(function (val, index) {
-				return React.createElement('div', {key: index}, val);
+				return React.createElement('div', {key: index, className: 'record'}, [
+					React.createElement('div', {key: 1, className: 'record-avatar column two'}, [
+						React.createElement('img', {key: 1, src: 'img/avatar.png'})
+					]),
+					React.createElement('div', {key: 2, className: 'record-data columns ten'}, [
+						React.createElement('div', {key: 1, className: 'record-name'}, [
+							React.createElement('span', {key: 1}, 'Name '),
+							React.createElement('span', {key: 2}, 'Surname')
+						]),
+						React.createElement('div', {key: 2, className: 'record-id'}, [
+							React.createElement('span', {key: 1}, val)
+						]),
+						React.createElement('div', {key: 3, className: 'record-desc'}, [
+							React.createElement('span', {key: 1}, 'Description')
+						]),
+						React.createElement('div', {key: 4, className: 'record-sm'}, [
+							React.createElement('a', {key: 1, href: 'https://www.facebook.com/appdy.net'}, [
+								React.createElement('img', {key: 1, src: 'img/facebook.png'})
+							]),
+							React.createElement('a', {key: 5, href: 'https://twitter.com/AppdyApp'}, [
+								React.createElement('img', {key: 1, src: 'img/twitter.png'})
+							]),
+							React.createElement('a', {key: 6, href: 'https://linkedin.com'}, [
+								React.createElement('img', {key: 1, src: 'img/linkedin.png'})
+							])
+						])
+					])
+				]);
 			});
 
 			return React.createElement('div', {}, elements);
