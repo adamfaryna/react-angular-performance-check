5c26b6f4ed7b5fbbc943fdd7ff907119aedc2986
Adam Faryna <adamfaryna@gmail.com>
2016-11-22 18:36:49 +0000

    Prepare view test.

---
+++ test/indexView.spec.js
@@ -0,0 +1,17 @@
+const pug = require('pug');
+const jsdom = require('jsdom-global');
+const expect = require('chai').expect;
+
+jsdom(pug.renderFile('views/index.pug'));
+
+describe('index view', () => {
+	it('should contain proper header');
+	it('should contain proper sample record block');
+	it('should contain proper, hidden progress bar');
+	it('should contain proper Angular experiment block');
+	it('should contain proper React experiment block');
+	it('should contain proper Vanilla experiment block');
+	it('should contain proper Angular-React experiment block');
+	it('should contain proper, hidden raports block');
+	it('should contain proper footer');
+});
