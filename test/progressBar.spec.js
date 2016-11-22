d8ad1a55c1a5c93d67539e4189b731372f3602ad
Adam Faryna <adamfaryna@gmail.com>
2016-11-22 18:37:11 +0000

    Prepare progress bar test.

--- test/progressBar.spec.js
+++ test/progressBar.spec.js
@@ -2,18 +2,23 @@ const pug = require('pug');
 const jsdom = require('jsdom-global');
 const expect = require('chai').expect;
 
+// global.document = 
 jsdom(pug.renderFile('views/index.pug'));
+// global.document = window.document;
+// global.window = global.document.defaultView;
+// global.navigator = window.navigator = {};
+// global.Node = window.Node;
 
 const progressBar = require('../public/javascripts/progressBar');
 
 describe('progressBar', () => {
 	it('should show up', () => {
 		progressBar.show();
-		expect(document.getElementById('progressBar').classList.contains('hidden')).to.be.false;
+		expect(global.document.getElementById('progressBar').classList.contains('hidden')).to.be.false;
 	});
 
 	it('should hide', () => {
 		progressBar.hide();
-		expect(document.getElementById('progressBar').classList.contains('hidden')).to.be.true;
+		expect(global.document.getElementById('progressBar').classList.contains('hidden')).to.be.true;
 	});
 });
